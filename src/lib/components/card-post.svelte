445346bbb6d7b78d8bdfae894b5ac6a172b12306
indra <indrasaputraidrus@gmail.com>
2024-04-06 16:23:29 +0800

    add card post

---
+++ src/lib/components/card-post.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+</script>
+
+<Card.Root>
+	<Card.Header>
+		<Card.Title>My post</Card.Title>
+	</Card.Header>
+	<Card.Content>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci explicabo nihil et
+			ipsa reprehenderit nisi modi necessitatibus sed expedita?
+		</p>
+	</Card.Content>
+</Card.Root>
